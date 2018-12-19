e9c5b7562f822957329d6536811a4b6eac1e205c
randy <tmps880305@gmail.com>
2018-12-19 12:02:07 +0800

    randy --hide img in member

--- src/main/resources/static/js/formatStrap.js
+++ src/main/resources/static/js/formatStrap.js
@@ -1,12 +1,32 @@
+
+function showCross(id) {
+    var tmp = id+"Pic"
+    var image = document.getElementById(tmp);
+    image.src = "/img/cross_icon.png";
+    image.width = 20;
+    image.height = 20;
+    image.style = "";
+}
+
+function showCheck(id) {
+    var tmp = id+"Pic"
+    var image = document.getElementById(tmp);
+    image.src = "/img/icon-check.png";
+    image.width = 20;
+    image.height = 20;
+    image.style = "";
+}
+
 function isName(id) {
     var name_element = document.getElementById(id);
     var name = name_element.value;
-    document.getElementById("nameCheck").style.display = "none";
+    var tmpCk = id+"Check";
+    var tmpCr = id+"Cross";
+
     if (name != "aaa01") {
-        document.getElementById("nameCross").style = "";
+        showCross(id);
     } else {
-        document.getElementById("nameCross").style.display = "none";
-        document.getElementById("nameCheck").style = "";
+        showCheck(id);
     }
 }
 
@@ -14,12 +34,12 @@ function isAccount(id) {
     var account_element = document.getElementById(id);
     var account = account_element.value;
     //should be correct formation
-    document.getElementById("accountCheck").style.display = "none";
+
+
     if (account != "aaa01") {
-        document.getElementById("accountCross").style = "";
+        showCross(id);
     } else {
-        document.getElementById("accountCross").style.display = "none";
-        document.getElementById("accountCheck").style = "";
+        showCheck(id);
     }
 }
 
@@ -27,26 +47,22 @@ function isPassword(id) {
     var password_element = document.getElementById("newPassword");
     var password = password_element.value;
     //should be correct formation and length
-    document.getElementById("passCheck").style.display = "none";
     if (password != "123") {
-        document.getElementById("passCross").style = "";
+        showCross(id);
     } else {
-        document.getElementById("passCross").style.display = "none";
-        document.getElementById("passCheck").style = "";
+        showCheck(id);
     }
 }
 
 function checkPassword(id) {
     var password1_element = document.getElementById("newPassword");
     var password1 = password1_element.value;
-    var password2_element = document.getElementById("cfmPassword");
+    var password2_element = document.getElementById(id);
     var password2 = password2_element.value;
 
-    document.getElementById("pass2Check").style.display = "none";
     if (password1 != password2) {
-        document.getElementById("pass2Cross").style = "";
+        showCross(id);
     } else {
-        document.getElementById("pass2Cross").style.display = "none";
-        document.getElementById("pass2Check").style = "";
+        showCheck(id);
     }
 }
